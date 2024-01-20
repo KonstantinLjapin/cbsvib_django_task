abf3225ab9e1b25db5f0f0073033151e29c06f96
konstantin_liapin <lajpinkonstantin@gmail.com>
2024-01-20 13:09:24 +0300

    django_start,db

---
+++ test_start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# need chmod +
+sudo docker compose up;
+sudo docker stop $(sudo docker ps -a -q);
+sudo docker rm $(sudo docker ps -a -q);
+sudo docker rmi $(sudo docker images --format="{{.Repository}} {{.ID}}" |
+                  grep "^cbsvib_django_task-django" | cut -d' ' -f2);
